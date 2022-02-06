45bea96b7b997f0a6915f3955dd5a6ffc1cd904d
Mohammad Shariq <shariqsway@gmail.com>
2022-02-07 04:33:01 +0530

    Prototypes Created
    Created prototypes in VueJs and Blazor.

---
+++ Assignment 1/CollegeBlazorApp/CollegeBlazorApp/Shared/Applicant.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeBlazorApp.Server.Models
+{
+    public class Applicant
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Applicant ID")]
+        public string ApplicantId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+    }
+}
---
+++ Assignment 1/CollegeBlazorApp/CollegeBlazorApp/Shared/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CollegeBlazorApp.Server.Models
+{
+    public class Program
+    {
+        public int Id { get; set; }
+        public int ProgramCode { get; set; }
+        public string ProgramDescription { get; set; }
+    }
+}
